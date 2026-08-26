c5730104f66a16e0a51f5d4fc582fa6fa6255fb6
pw-5214 <prathmeshwavhal83@gmail.com>
2026-08-26 16:24:04 +0530

    Fix merge conflicts and production build

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
   reactStrictMode: false,
   turbopack: {
     root: __dirname,
   },
-=======
-  /* config options here */
->>>>>>> 52fc6d6b1d321253741e9249f27c7a76ea218d59
 };
 
 export default nextConfig;
